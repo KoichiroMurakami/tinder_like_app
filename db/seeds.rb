ffa14d12c05db280ccf5eff0765dbaf49ed36b14
Rick516 <k.murakami516@gmail.com>
2020-04-21 12:55:11 +0900

    dummy data created

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,37 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+p "create Users"
+User.create!(
+    email: 'user1@example.com',
+    password: 'aaaaaaaa',
+    name: '里彩',
+    self_introduction: '里彩です',
+    sex: 0,
+    img_name: open("#{Rails.root}/db/dummy_img/1.jpg")
+)
+User.create!(
+    email: 'user2@example.com',
+    password: 'aaaaaaaa',
+    name: 'risa murakami',
+    self_introduction: 'ハリーです',
+    sex: 0,
+    img_name: open("#{Rails.root}/db/dummy_img/2.jpg")
+)
+User.create!(
+    email: 'user3@example.com',
+    password: 'aaaaaaaa',
+    name: 'risa',
+    self_introduction: 'this is risa',
+    sex: 1,
+    img_name: open("#{Rails.root}/db/dummy_img/3.jpg")
+)
+User.create!(
+    email: 'user4@example.com',
+    password: 'aaaaaaaa',
+    name: 'risa4',
+    self_introduction: ' this is risa4',
+    sex: 1,
+    img_name: open("#{Rails.root}/db/dummy_img/4.jpg")
+)
